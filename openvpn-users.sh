65fe29f3ce4449f107c8cda37b2910e748c22985
Jeffrey Forman <jeff@jeffreyforman.net>
2010-10-28 16:33:36 -0400

    Add error handling for openvpn-users when status file not found.
    Updated README as well

---
+++ openvpn-users.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+## Munin plugin to return how many OpenVPN sessions are active.
+## Requirements for OpenVPN server config file:
+### status-file $foo
+### status-version 2
+## Requirements for 'openvpn-users' config stanza for munin-node:
+### [openvpn-users]
+### env.statusfile "/var/log/openvpn.stat"
+
+if [[ $1 == "config" ]]; then
+    echo "graph_title OpenVPN Users"
+    echo "graph_vlabel User Count"
+    echo "user.label Logged In Users"
+    exit 0
+fi
+
+OVPNSTATUSFILE=$statusfile
+
+if [ ! -e $OVPNSTATUSFILE ]; then
+    echo "OpenVPN status file $OVPNSTATUSFILE does not exist"
+    exit 1
+fi
+
+USERCOUNT=`grep ^CLIENT_LIST $OVPNSTATUSFILE | wc -l`
+
+echo "user.count $USERCOUNT"
+exit 0
