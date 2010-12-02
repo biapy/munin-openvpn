b110de711f48dafa6ab46ecc7a4cd41a0d71eee3
Jeffrey Forman <jeff@jeffreyforman.net>
2010-12-02 15:30:37 -0500

    A working per-user network transfer plugin for openvpn
    Still need to environmentalize the status log

--- openvpn-usertransfer.sh
+++ openvpn-usertransfer.sh
@@ -8,23 +8,29 @@ client_list="`grep ^CLIENT_LIST $statusfile`"
 
 if [[ $1 == "config" ]]; then
     echo "graph_title OpenVPN User Transfer Rate"
-    echo "graph_order down up"
     echo "graph_vlabel Bytes Received / Bytes Sent"
     echo "graph_category openvpn"
+    echo "graph_info Show the data transfer over the VPN per user."
     for current_line in $client_list; do
         current_user=`echo $current_line | cut -d, -f2`
-        echo "$current_user.label $current_user"
-        echo "$current_user.type COUNTER"
-        echo "${current_user}_down.graph no"
-        echo "${current_user}_up.negative down"
+        echo "${current_user}_received.label ${current_user}"
+        echo "${current_user}_received.type DERIVE"
+	echo "${current_user}_received.min 0"
+	echo "${current_user}_received.graph no"
+	echo "${current_user}_received.draw LINE2"
+        echo "${current_user}_sent.label ${current_user}"
+        echo "${current_user}_sent.type DERIVE"
+	echo "${current_user}_sent.min 0"
+	echo "${current_user}_sent.negative ${current_user}_received"
+	echo "${current_user}_sent.draw LINE2"
     done
     exit 0
 fi
 
 for current_line in $client_list; do
     current_user=`echo $current_line | cut -d, -f2`
-    current_dl=`echo $current_line | cut -d, -f5`
-    echo "${current_user}_down.value  $current_dl"
-    current_ul=`echo $current_line | cut -d, -f6`
-    echo "${current_user}_up.value $current_ul"
+    current_received=`echo $current_line | cut -d, -f5`
+    current_sent=`echo $current_line | cut -d, -f6`
+    echo "${current_user}_received.value $current_received"
+    echo "${current_user}_sent.value $current_sent"
 done
